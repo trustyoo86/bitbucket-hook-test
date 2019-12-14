fde7cfd9639af0d63ddc066d23f1dc6e95e9050c
trustyoo86 <trustyoo86.github.io>
2019-12-14 23:24:08 +0900

    Add post router

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const express = require('express');
 
 const app = express();
 
+app.post('/hook', (req, res) => {
+  console.log('req', req);
+  res.send({ msg: 'success' });
+});
+
 app.listen(80, () => {
   console.log('hook server listen in 80');
 });
