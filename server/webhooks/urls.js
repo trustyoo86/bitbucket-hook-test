9f64e930efa5a217805f663bdcc64210aa2187f9
유한승 <kern86@wemakeprice.com>
2020-08-18 01:07:14 +0900

    feat(BS-3): Add environment urls
    * Add environment urls

--- server/webhooks/urls.js
+++ server/webhooks/urls.js
@@ -2,6 +2,9 @@
 
 const urls = {
   development: 'https://outlook.office.com/webhook/5d08afd4-de0a-459b-bb43-3284add50fa8@6322d5f5-2204-4e9d-9ca6-d18828a04daf/IncomingWebhook/26f31030d4e94cbeaac2091b25d8dde7/5ac8faad-928d-400f-b1ed-33ff84d92f8c',
+  qa: '',
+  staging: '',
+  production: '',
 };
 
 module.exports = urls;
